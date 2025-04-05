fdb5b019fab707443bae410ec2941ad6bebb8dc6
djoleluk <djoleluk@gmai.com>
2025-04-05 11:27:52 +0200

    New commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/home/Home'
+import { BrowserRouter as Router, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <Routes>
-          <Route path='/' element={<Home/>}/>
+          
       </Routes>       
     </Router>
   )
